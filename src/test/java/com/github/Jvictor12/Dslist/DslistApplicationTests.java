cb0eaa0a4f85364fc43f336c8bf8102644b35e19
Jvictor12 <01joaovictordvp@gmail.com>
2023-05-08 22:18:00 -0300

    First Commit API for studies

---
+++ src/test/java/com/github/Jvictor12/Dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Jvictor12.Dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
